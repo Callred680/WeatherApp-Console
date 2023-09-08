c1b0d8ecfefd0decebf3f0522f08cdd892c3f1d3
Connor Allred <Callred680@gmail.com>
2023-09-07 20:46:35 -0500

    Current weather function + better formatting

--- Weather.java
+++ Weather.java
@@ -1,18 +1,129 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Weather{
+    // Each enum type corresponds to one specifc API call that can be made
+    enum SearchMethod{
+        CURRENT("/current.xml?key="),    // For obtaining realtime weather
+        FORECAST("/forecast.xml?key="),   // For obtaining hourly weather for next 3 days (dependent on price level, up to 14 days)
+        FUTURE("/future.xml?key="),     // For obtaining hourly weather for day atleast 14 days in the future   (unavailable with current price plan)
+        HISTORY("/history.xml?key="),    // For obtaining weather from the past 7 days (at most)
+        MARINE("/marine.xml?key="),     // For obtaining realtime marine weather
+        ASTRONOMY("/astronomy.xml?key="),  // For obtaining realtime astornomy info
+        IP("/ip.xml?key="),         // For obtaining up to date information based on IP address
+        TIMEZONE("/timezone.xml?key="),   // For obtaining time zone information for given input value
+        SEARCH("/search.xml?key=");     // For obtaining weather with incomplete/partial location input
 
+        // Constructor for creating enum with values (specific api method call)
+        private String method;  // Will be set to api method call for completing URI builder
+        private SearchMethod(String method){
+            this.method = method;
+        }
+        // Allowing main program to retrieve specifc method name
+        public String getMethod(){
+            return this.method;
+        }
+    }
+
+    /*
+     *  THIS PROGRAM ASSUMES ONLY VALID INPUT IS GIVEN
+     *      - ADDITIONAL CODE FOR INPUT VALIDATION TBD 
+     */
+    static Scanner input = new Scanner(System.in);
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
+        WeatherAPI GetWeather = new WeatherAPI("en", "no", "F", "MPH");   // Create WeatherAPI object with english as default language
 
-        System.out.print("Enter zipcode: ");
-        String zipcode = input.nextLine();
-        System.out.print("Enter language: ");
-        String lang = input.nextLine();
+        DisplayMenu();
+        String selection = input.nextLine();
+        selection = selection.toUpperCase();    // Case Sensitive so eliminate potential input error for selection choice
 
-        WeatherAPI GetWeather = new WeatherAPI(zipcode, lang);
-        GetWeather.GetCurrentData();
+        while(!selection.equals("-1") && !selection.equals("EXIT")){
+            // Send API request with inputted parameters from user and selection choice for correct API call method
+            if(!selection.equals("SETTINGS")){    // Represents settings
+                GetWeather.GetData(SearchMethod.valueOf(selection), getParameters(SearchMethod.CURRENT));
+                System.out.println();
+            }else{
+                Settings(GetWeather);
+                System.out.println("==========================================");
+            }
+            DisplayMenu();
+            selection = input.nextLine().toUpperCase();
+        }
+
         input.close();
+    }
 
+    private static void DisplayMenu(){
+        System.out.println("Welcome! Please select one of the options below (Type out selection EX: current)\n");
+        SearchMethod[] methods = SearchMethod.values();
+        for(int i = 0; i < methods.length; i++){
+            System.out.println((i+1) + ". " + methods[i]);  // Displays all possible methods (EX: 1. CURRENT)
+        }
+        System.out.print(0 + ". SETTINGS \n===> ");
+    }
+
+    private static void Settings(WeatherAPI WeatherAPISettings){
+        System.out.println("Select which option to change (Enter number)\n==========================\n");
+        System.out.println("1. Language\n2. AQI\n3. Temperature units\n4. Speed Units\n5. Exit\n0. Back");
+
+        switch(input.nextLine()){
+            case "1":
+                System.out.print("Enter desired language (language code) => "); // Lower case for api call parameter use
+                WeatherAPISettings.setLanguage(input.nextLine());
+                break;
+            case "2":
+                System.out.print("AQI included, yes or no? => ");
+                WeatherAPISettings.setLanguage(input.nextLine().toLowerCase()); // Lower case for api call parameter use
+                break;
+            case "3":
+                System.out.print("Fahrenheit(F) or Celsius(C) => ");
+                WeatherAPISettings.setLanguage(input.nextLine().toUpperCase()); // Upper case for when being displayed
+                break;
+            case "4":
+                System.out.print("Miles Per Hour(MPH) or Kilometers Per Hour(KPH) => ");
+                WeatherAPISettings.setLanguage(input.nextLine().toUpperCase()); // Upper case for when being displayed
+                break;
+            case "5":
+                input.close();
+                System.exit(0); // Exit choice was selected so end program
+                break;
+            case "0":
+                // For "going back" to menu
+                break;
+        }
+    }
+
+
+    private static ArrayList<String> getParameters(SearchMethod search){
+        ArrayList<String> params = new ArrayList<String>();
+        
+        // Obtain location to check weather (appended with correct formatting for making API call)
+        System.out.print("Please enter location: ");
+        params.add("&q=" + input.nextLine());
+
+        // If API call is one of these methods, additional paramter requests are needed from user
+        switch(search){
+            case FORECAST:
+                System.out.print("Please enter number of days for forecast(1 to 14): ");
+                params.add("&days=" + input.nextLine());
+                break;
+            case FUTURE:
+                break;
+            case HISTORY:
+                break;
+            case MARINE:
+                break;
+            case ASTRONOMY:
+                break;
+            case IP:
+                break;
+            case TIMEZONE:
+                break;
+            case SEARCH:
+                break;
+            default:
+
+        }
+        return params;
     }
 }
--- WeatherAPI.java
+++ WeatherAPI.java
@@ -1,54 +1,124 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.StringReader;
+
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 
 public class WeatherAPI {
 
     private static final String Key = getKey(); // Establish API key for user
-    private static final String URI = "http://api.weatherapi.com/v1";   // Base URI for being able to select desired information when requested
-    private static final String currentMethod = "/current.xml?key=";
-
-    private String zipcode = "&q=", lang;
+    private static final String BaseURL = "http://api.weatherapi.com/v1";   // Base URI for being able to select desired information when requested
 
     public HttpRequest request = null;
     public HttpResponse<String> response = null;
 
+    private String Language, AQI, TempUnit, SpeedUnit; 
     // intializes class with default values for getting weather
-    WeatherAPI(String zipcode, String lang){
-        this.zipcode += zipcode;
-        this.lang = lang;
+    WeatherAPI(String Language, String AQI, String TempUnit, String SpeedUnit){
+        this.Language = Language;
+        this.AQI = AQI;
+        this.TempUnit = TempUnit;
+        this.SpeedUnit = SpeedUnit;
+        
     };
 
-    // Fetches data for current weather
-    public void GetCurrentData(){
-        try {
+    // Fetches data for current weather data
+    public void GetData(Weather.SearchMethod search, ArrayList<String> params){
+        try{
+            String addParams = "";
+            for(String p : params){
+                addParams += p;
+            }
+            URI uri = new URI(BaseURL + search.getMethod() + Key + addParams + "&lang=" + Language + "&aqi=" + AQI);
             request = HttpRequest.newBuilder()
-                .uri(new URI(URI+currentMethod+Key+zipcode+"&aqi=no"))  // Sets the HttpRequest's request URI (Uniform Resource Identifier, char sequence to identify which logical or physical resource)
+                .uri(uri)  // Sets the HttpRequest's request URI (Uniform Resource Identifier, char sequence to identify which logical or physical resource)
                 .header("Authorization", Key)   // .GET() is the default 
                 .build();
-            try {
-                response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-                if(response == null){
-                    System.out.println("ERROR! Request time out...");
-                }else{
-                    displayData(response.body());                    
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
+            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            if(response.statusCode() != 200){
+                System.out.println("ERROR! Request time out...");
+            }else{
+                displayData(readData( response.body() ), search);   // Reads xml string into document which is then displayed
             }
-        }catch (Exception e) {
+        }catch(Exception e){
             e.printStackTrace();
         }
     }
 
-    // Function for displaying each header returned in .json file depedent on request method called
-    public void displayData(String data){
-        System.out.println(data);
+    // Function for processing data received from request call (XML file)
+    private Document readData(String XML) throws SAXException, IOException, ParserConfigurationException{
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(new InputSource(new StringReader(XML)));
+
+        // Puts all text nodes underneath a node (neither adjacent Text nodes nor empty ones)
+        document.getDocumentElement().normalize();
+
+        return document;
+    }
+
+    // Function for displaying information for the inputted location as well as respective information for inputted search method
+    private void displayData(Document doc, Weather.SearchMethod search){
+        Element root = doc.getDocumentElement();
+        System.out.println(root.getNodeName());
+
+        // Showing weather for one location only so do not need for loop
+        NodeList list = doc.getElementsByTagName("location");
+        Element node = (Element) list.item(0);  // Receives first item in list (only one item since its location)
+        System.out.println("==========================================");
+        System.out.printf("City : %s%n", node.getElementsByTagName("name").item(0).getTextContent());
+        System.out.printf("Region : %s%n", node.getElementsByTagName("region").item(0).getTextContent());
+        System.out.printf("Country : %s%n", node.getElementsByTagName("country").item(0).getTextContent());
+        System.out.printf("Latitude : %s\t  Longitude : %s%n", node.getElementsByTagName("lat").item(0).getTextContent(), node.getElementsByTagName("lon").item(0).getTextContent());
+        System.out.printf("Timezone : %s%n", node.getElementsByTagName("tz_id").item(0).getTextContent());
+        System.out.printf("Local Time : %s%n", node.getElementsByTagName("localtime").item(0).getTextContent());
+
+        switch(search){
+            case CURRENT:
+                list = doc.getElementsByTagName("current");
+                node = (Element)list.item(0);
+                System.out.println("==========================================");
+                System.out.printf("Last Updated : %s%n", node.getElementsByTagName("last_updated").item(0).getTextContent());
+                System.out.printf("Temperature : %s\u00B0%s%n", node.getElementsByTagName((TempUnit.equals("F") ? "temp_f" : "temp_c")).item(0).getTextContent(), TempUnit); // Embedded if statement for selecting which parameter to display (Celsius or Fahrenheit)
+                System.out.printf("Feels Like : %s\u00B0%s%n", node.getElementsByTagName((TempUnit.equals("F") ? "feelslike_f" : "feelslike_c")).item(0).getTextContent(), TempUnit); // Embedded if statement for selecting which parameter to display (Celsius or Fahrenheit)
+                System.out.printf("UV Index : %s%n", node.getElementsByTagName("uv").item(0).getTextContent());
+                System.out.printf("Wind Speed : %s%n", node.getElementsByTagName((SpeedUnit.equals("MPH") ? "wind_mph" : "feelslike_c")).item(0).getTextContent()+" "+SpeedUnit); // Embedded if statement for selecting which parameter to display (MPH or KPH)
+                System.out.printf("Humidity : %s%n", node.getElementsByTagName("humidity").item(0).getTextContent());
+                break;
+            case FORECAST:
+                break;
+            case FUTURE:
+                break;
+            case HISTORY:
+                break;
+            case MARINE:
+                break;
+            case ASTRONOMY:
+                break;
+            case IP:
+                break;
+            case TIMEZONE:
+                break;
+            case SEARCH:
+                break;
+            default:
+        }
     }
 
     // Serves to read api key from .txt file to be used for authorization in api calls [rather than hard coding]
@@ -60,5 +130,18 @@ public class WeatherAPI {
             e.printStackTrace();
         }
         return "0";
+    }
+
+    public void setLanguage(String lang){
+        this.Language = lang;
+    }
+    public void setAQI(String AQI){
+        this.AQI = AQI;
+    }
+    public void setTempUnit(String Unit){
+        this.TempUnit = Unit;
+    }
+    public void setSpeedUnit(String Unit){
+        this.SpeedUnit = Unit;
     }
 }
